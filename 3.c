9a1c40b817b42fa9e9d329a4c1124b43e2f72afc
huawenyao <1503642213@qq.com>
2021-03-08 14:20:40 +0800

    1.0.3

---
+++ 3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("xihuanxi \n");
+	printf("xihuanxi \n");
+	printf("xihuanxi \n");
+	printf("xihuanxi \n");
+	printf("xihuanxi \n");
+	printf("xihuanxi \n");
+	printf("xihuanxi \n");
+	return 0;
+}
